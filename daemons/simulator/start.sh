e2e4450a9135b3d3f8de0e08ba1d54cc7ee5ea7f
Daniele Viganò <daniele@vigano.me>
2022-01-03 22:14:11 +0100

    Improve sim container

--- daemons/simulator/start.sh
+++ daemons/simulator/start.sh
@@ -2,11 +2,12 @@
 
 PTY=0
 
+# if container is run with -ti pts/0 is already taken
 if [ -c /dev/pts/0 ]; then
     PTY=1
 fi
 
 sed -i "s/ttyACM0/pts\/${PTY}/" /opt/dcc/config.ini
 
-qemu-system-avr -machine uno -bios /io/CommandStation-EX-uno-*.elf -serial pty -daemonize
+qemu-system-avr -machine uno -bios /io/CommandStation-EX*.elf -serial pty -daemonize
 /opt/dcc/net-to-serial.py
