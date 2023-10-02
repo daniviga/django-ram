22bee7d95d1973c4ad863140457c0caebe8732f2
Daniele Viganò <daniele@vigano.me>
2023-10-02 23:16:54 +0200

    Show booshelf menu

--- ram/portal/templatetags/show_menu.py
+++ ram/portal/templatetags/show_menu.py
@@ -1,10 +1,16 @@
 from django import template
-from portal.views import Flatpage
+from portal.models import Flatpage
+from bookshelf.models import Book
 
 register = template.Library()
 
 
+@register.inclusion_tag('bookshelf_menu.html')
+def show_bookshelf_menu():
+    return {"bookshelf_menu": Book.objects.exists()}
+
+
 @register.inclusion_tag('flatpage_menu.html')
-def show_menu():
+def show_flatpage_menu():
     menu = Flatpage.objects.filter(published=True).order_by("name")
-    return {"menu": menu}
+    return {"flatpage_menu": menu}
