d45170cf1e804fdbc1efac98a3a2fe064ccfd993
Daniele Viganò <daniele@vigano.me>
2024-02-17 18:29:51 +0100

    Reformat files

--- ram/ram/views.py
+++ ram/ram/views.py
@@ -7,21 +7,19 @@ from PIL import Image, UnidentifiedImageError
 
 from django.views import View
 from django.conf import settings
-from django.http import (
-    JsonResponse, HttpResponseForbidden, HttpResponse
-)
+from django.http import HttpResponse, HttpResponseForbidden, JsonResponse
 from django.utils.text import slugify as slugify
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
 
-@method_decorator(csrf_exempt, name='dispatch')
+@method_decorator(csrf_exempt, name="dispatch")
 class UploadImage(View):
     def post(self, request, application=None, model=None):
         if not request.user.is_authenticated:
             raise HttpResponseForbidden()
 
-        file_obj = request.FILES['file']
+        file_obj = request.FILES["file"]
         file_name, file_extension = os.path.splitext(file_obj.name)
         file_name = slugify(file_name) + file_extension
 
@@ -40,20 +38,17 @@ class UploadImage(View):
             today.strftime("%d"),
         )
 
-        file_path = os.path.join(
-            settings.MEDIA_ROOT,
-            *(p for p in container)
-        )
+        file_path = os.path.join(settings.MEDIA_ROOT, *(p for p in container))
         Path(file_path).mkdir(parents=True, exist_ok=True)
-        with open(os.path.join(file_path, file_name), 'wb+') as f:
+        with open(os.path.join(file_path, file_name), "wb+") as f:
             for chunk in file_obj.chunks():
                 f.write(chunk)
 
-            return JsonResponse({
-                'message': 'Image uploaded successfully',
-                'location': posixpath.join(
-                    settings.MEDIA_URL,
-                    *(p for p in container),
-                    file_name
-                )
-            })
+            return JsonResponse(
+                {
+                    "message": "Image uploaded successfully",
+                    "location": posixpath.join(
+                        settings.MEDIA_URL, *(p for p in container), file_name
+                    ),
+                }
+            )
