df24088e7d98331336f811f5c495ab72a6a92ef1
Daniele Viganò <daniele@vigano.me>
2024-11-04 14:57:05 +0100

    Use uuid in urls.py

--- ram/bookshelf/urls.py
+++ ram/bookshelf/urls.py
@@ -3,5 +3,5 @@ from bookshelf.views import BookList, BookGet
 
 urlpatterns = [
     path("book/list", BookList.as_view()),
-    path("book/get/<str:uuid>", BookGet.as_view()),
+    path("book/get/<uuid:uuid>", BookGet.as_view()),
 ]
