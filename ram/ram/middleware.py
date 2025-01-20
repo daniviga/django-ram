3520a8717177124620da6be319859b9cfe596ddf
Daniele Viganò <daniele@vigano.me>
2025-01-20 16:50:44 +0100

    Add X-Cache-Hit header

--- ram/ram/middleware.py
+++ ram/ram/middleware.py
@@ -1,4 +1,5 @@
-from django.utils.cache import add_never_cache_headers
+from django.core.cache import cache
+from django.utils.cache import add_never_cache_headers, get_cache_key
 
 
 class DisableClientSideCachingMiddleware:
@@ -7,5 +8,13 @@ class DisableClientSideCachingMiddleware:
 
     def __call__(self, request):
         response = self.get_response(request)
+
+        # Check if the cache key exists for this request
+        cache_key = get_cache_key(request)
+        cache_hit = "MISS"
+        if cache_key and cache.get(cache_key):
+            cache_hit = "HIT"
+        response['X-Cache-Hit'] = cache_hit
+
         add_never_cache_headers(response)
         return response
