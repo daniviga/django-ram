935c4390848669bdf960f98f5c16e19a0e487029
Daniele Viganò <daniele@vigano.me>
2025-12-03 23:07:56 +0100

    Introduce compatibility with Django 6.0

--- ram/portal/templatetags/dcc.py
+++ ram/portal/templatetags/dcc.py
@@ -1,36 +1,38 @@
 from django import template
 from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 
 @register.simple_tag
 def dcc(object):
-    socket = (
+    socket = mark_safe(
         '<i class="bi bi-ban small"></i>'
     )
     decoder = ''
     if object.decoder_interface is not None:
-        socket = (
+        socket = mark_safe(
             f'<abbr title="{object.get_decoder_interface()}">'
             f'<i class="bi bi-dice-6"></i></abbr>'
         )
     if object.decoder:
         if object.decoder.sound:
-            decoder = (
+            decoder = mark_safe(
                 f'<abbr title="{object.decoder}">'
                 '<i class="bi bi-volume-up-fill"></i></abbr>'
             )
         else:
-            decoder = (
+            decoder = mark_safe(
                 f'<abbr title="{object.decoder}'
                 f'({object.get_decoder_interface()})">'
                 '<i class="bi bi-cpu-fill"></i></abbr>'
             )
-
     if decoder:
         return format_html(
-            f'{socket} <i class="bi bi-arrow-bar-left"></i>{decoder}'
+            '{} <i class="bi bi-arrow-bar-left"></i> {}',
+            socket,
+            decoder,
         )
 
-    return format_html(socket)
+    return socket
